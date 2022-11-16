9e5346c52138208ce7f0a9dfc3ef8bf4900a236d
DoubleK <airvw14@gmail.com>
2022-11-16 23:13:27 +0900

    test: getTickers by using query string with comma(,)

--- src/test/java/TickerTest.java
+++ src/test/java/TickerTest.java
@@ -1,11 +1,11 @@
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import utils.HttpSetting;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,16 +19,27 @@ public class TickerTest {
     @Test
     @DisplayName("시세 확인 테스트")
     void tickerNow() throws IOException {
-        OkHttpClient client = new OkHttpClient();
+//        OkHttpClient client = new OkHttpClient();
+//        Request request = new Request.Builder()
+//                .url("https://api.upbit.com/v1/ticker?markets=KRW-BTC")
+//                .get()
+//                .addHeader("accept", "application/json")
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//        System.out.println(response.body().string());
+        String resultStr = HttpSetting.getKRWTicker("KRW-BTC");
+        assertThat(resultStr).isNotNull();
+    }
 
-        Request request = new Request.Builder()
-                .url("https://api.upbit.com/v1/ticker?markets=KRW-BTC")
-                .get()
-                .addHeader("accept", "application/json")
-                .build();
-
-        Response response = client.newCall(request).execute();
-        System.out.println(response.body().string());
-        assertThat(response.body()).isNotNull();
+    @Test
+    @DisplayName("여러 코인 시세 확인 테스트 -> 414 Request-URI Too Large 오류")
+    void getTickers() throws IOException{
+        String coinStr = HttpSetting.getKRWCoinList();
+        String resultStr = HttpSetting.getKRWTicker(coinStr);
+        System.out.println(resultStr);
+        JSONArray jsonArray = new JSONArray(resultStr);
+        IntStream.range(0, resultStr.length()).mapToObj(jsonArray::getJSONObject)
+                .forEach(oj -> System.out.println(oj.getString("trade_price")));
     }
 }
